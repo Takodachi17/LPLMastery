41e90398e3293b976f62172834b4c8c8a4787bf9
Takodachi17 <94741053+Takodachi17@users.noreply.github.com>
2023-06-06 20:17:21 +0800

    Update contact.php

--- contact.php
+++ contact.php
@@ -5,20 +5,16 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $email = $_POST["email"];
     $message = $_POST["message"];
    
-
     $to = "halconlance17@gmail.com"; 
     
-
     $subject = "New message from LPL Mastery contact form";
     
-
     $emailContent = "Name: $name\n";
     $emailContent .= "Email: $email\n\n";
     $emailContent .= "Message:\n$message";
     
     $headers = "From: $name <$email>\r\n";
     
- 
     if (mail($to, $subject, $emailContent, $headers)) {
         echo "Thank you! Your message has been sent.";
     } else {
